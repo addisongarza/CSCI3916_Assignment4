2cbd03dc00a37ea1c75b9f4f38f0bdd7c677adbd
Addison Garza <114454304+addisongarza@users.noreply.github.com>
2023-04-22 23:06:37 -0600

    Update Movies.js

--- CSC3916_Assignment4/Movies.js
+++ CSC3916_Assignment4/Movies.js
@@ -4,36 +4,35 @@ var bcrypt = require('bcrypt-nodejs');
 
 mongoose.Promise = global.Promise;
 
-//mongoose.connect(process.env.DB, { useNewUrlParser: true });
 try {
     mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
-        console.log("connected"));
+        console.log("connected - movies"));
 }catch (error) {
-    console.log("could not connect");
+    console.log("could not connect - movies");
 }
 mongoose.set('useCreateIndex', true);
 
-//movie schema
+//user schema
 var MovieSchema = new Schema({
-    title: { type: String, required: true, index: {unique: true }},
-    year_released: { type: String, required: true},
-    genre:
-        {
-            type: String,
-            required: true,
-            enum: ["Action", "Adventure", "Comedy", "Drama", "Fantasy", "Horror", "Mystery", "Thriller", "Western"]
-        },
-    actors:
-        [
-            { actor_name: { type: String, required: true}, character_name: { type: String, required: true}},
-            { actor_name: { type: String, required: true}, character_name: { type: String, required: true}},
-            { actor_name: { type: String, required: true}, character_name: { type: String, required: true}}
-        ],
-    imageUrl: { type: String }
+   title: {type: String, required: true, index: { unique: true }},
+   year: {type: String, required: true},
+   genre: {type: String, required: true},
+   cast: [{
+       actor: {type: String, required: true},
+       character: {type: String, required: true}
+   }]
 });
 
 MovieSchema.pre('save', function(next) {
-    next();
+   var movie = this;
+
+   if (movie.cast.length < 3) {
+       var err = new ValidationError(this);
+       err.errors.movie = new ValidatorError('need 3 or more actors in cast');
+       next(err);
+   } else {
+       next();
+   }
 });
 
 //return the model to server
